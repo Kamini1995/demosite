46df34547ae00c5b4af459d5e4f2ddb275cc8318
Kamini1995 <109232464+Kamini1995@users.noreply.github.com>
2023-07-29 13:46:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function openNav() {
+   var nav = document.getElementById("myNav");
+   nav.classList.add("active");
+  }
+  function closeNav() {
+    var nav = document.getElementById("myNav");
+    nav.classList.remove("active");
+  }
